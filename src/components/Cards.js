28ab5d12c6677d9700214849a4babb4aa52308a8
Feda Zidan <fido311@gmail.com>
2020-12-12 21:10:29 -0600

    Still working on the images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,21 +10,21 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='./assets/img/employeeDirectory.gif'
+              src='img/employeeDirectory.gif'
               text="This app is an employee directory. In this assignment we broke the application 
               to components then managed the components' respond to user events."
               label='Employee Directory'
               href='https://feda2020.github.io/CodeRepo-HM19-Employee-Directory'
             />
             <CardItem
-              src='/assets/img/budget-tracker.gif'
+              src='./assets/img/budget-tracker.gif'
               text='This budget tracker application use Mongo database and caching to allow for offline access and 
               functionality.'
               label='Budget Tracker'
               path='/portfolio'
             />
             <CardItem
-              src='/assets/img/HerokuApp.gif'
+              src='./assets/img/HerokuApp.gif'
               text="This application allows the user to write and store notes. The user can also delete the 
               notes they don't need anymore."
               label='Note Tacker'
@@ -33,21 +33,21 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='/assets/img/pizzaClawWalkTrough.gif'
+              src='./assets/img/pizzaClawWalkTrough.gif'
               text='This is a fun Pizza order application called Pizza Claw. The app was done with MySQL, Node, 
               Express, Handlebars and Passport.'
               label='Pizza Claw'
               path='/portfolio'
             />
             <CardItem
-              src='/assets/img/heroku-burger.gif'
+              src='./assets/img/heroku-burger.gif'
               text='This is a fun Burger Logger application allows the user to log in their favorite burgers. The app was done
               with MySQL, Handlebars and ORM.'
               label='Eat Da Burger!'
               path='/portfolio'
             />
             <CardItem
-              src='/assets/img/WeatherApp.gif'
+              src='./assets/img/WeatherApp.gif'
               text='Dashboard Weather App helps you as a traveler to search for different cities and check the weather 
               forecast and save your search.'
               label='Weather Dashboard'
