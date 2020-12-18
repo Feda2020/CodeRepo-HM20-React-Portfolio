57ae65b48f4c2e3f3d43a98d4acb7d81fd7491ba
Feda Zidan <fido311@gmail.com>
2020-12-17 19:59:28 -0600

    Working on the images

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -7,12 +7,13 @@ function CardItem(props) {
       <li className="cards__item">
         <Link className="cards__item__link" to={props.href}>
           <figure className="cards__item__pic-wrap" data-category={props.label}>
+            <a href={props.url}>
             <img
               className="cards__item__img"
               alt="Project Foto"
               src={props.src}
-              href={props.url}
             />
+            </a>
           </figure>
           <div className="cards__item__info">
             <h5 className="cards__item__text">{props.text}</h5>
